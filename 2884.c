c8ab63d450b1e198220d98b0b2a8a16919e9406e
LeeMinsup980603 <m1nsuppp@gmail.com>
2020-10-22 00:40:15 +0900

    If Statement

---
+++ 2884.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+    int M, H;
+    scanf("%d %d", &M, &H);
+
+    if(0 < M && H < 45) M -= 1, H += 15;
+    else if(0 < M && H >= 45) H -= 45;
+    else if(M == 0 && H < 45) M = 23, H +=15;
+    else if(M == 0 && H >= 45) H -= 45; 
+
+    printf("%d %d", M , H);
+
+
+
+    return 0;
+}
