3f2e53d4e4e7fc79b7d2e58cdb764b95c8fe921f
LeeMinsup980603 <m1nsuppp@gmail.com>
2020-10-29 13:54:57 +0900

    boj

--- 1316.c
+++ 1316.c
@@ -19,12 +19,45 @@ int main()
     scanf("%d", &N);
 
     char word[N][101];
+    int checker[26] ={ 0 };
+    int cnt = 0;
 
     for(int i = 0; i < N; i++)
+    {
         scanf("%s", word[i]);
+        for(int j = 0; j < 26; j++)
+            checker[j] = 0;
+        
+        for(int j = 0; j <strlen(word[i]); j++)
+        {
+            if(0 < j && word[i][j - 1] == word[i][j])
+            {
+                if(j == strlen(word[i]) - 1)
+                {
+                    cnt++;
+                    break;
+                }
+                else
+                    continue;
+            }
+            else{
+                if(checker[word[i][j] - 'a'] == 1)
+                    break;
+                else
+                {
+                    checker[word[i][j] - 'a'] = 1;
+                    if(j == strlen(word[i]) - 1)
+                    {
+                        cnt++;
+                        break;
+                    }
+                }    
+            }
+        }
+    }
 
+    printf("%d\n", cnt);    
 
-    
 
     return 0;
 }
